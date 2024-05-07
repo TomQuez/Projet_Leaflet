17058f6fe426e6094539285e38340bb08f32b221
Tom Quez <thomas.quezet@gmail.com>
2024-05-07 11:10:40 +0200

    ajout icon, constantes.tsx, travail sur MapContainer

---
+++ src/constantes.tsx
@@ -0,0 +1,35 @@
+export const townMarker=[
+    {
+        name:"Lapalisse",
+        coodinates:[46.2401,3.6339],
+        type:"town",
+        description:"peacefull town"
+    },
+    {
+        name:"vichy",
+        coordinates:[46.123,3.451],
+        type:"city",
+        description :"https://www.ville-vichy.fr/"
+    },
+    {
+        name:"Trézelles",
+        coordinates:[46.326651,3.592693],
+        type:"village",
+        description:"https://fr.wikipedia.org/wiki/Tr%C3%A9zelles"
+    },
+];
+
+export const iconMappings={
+    town:{
+        iconUrl:"../icon/town.png",
+        iconSize:[38,38],
+    },
+    city:{
+        iconUrl:"../icon/paysage-urbain.png",
+        iconSize:[38,38],
+    },
+    village:{
+        iconUrl:"../icon/village.png",
+        iconSize:[38,38],
+    }
+}
--- src/leafletMap.tsx
+++ src/leafletMap.tsx
@@ -1,19 +1,60 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState } from 'react';
+import { MapContainer,TileLayer, Marker, Popup } from 'react-leaflet';
+import {icon} from "leaflet";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import {iconMappings, townMarker} from "./constantes";
+import "./LeafletInfos.scss";
 
-const LeafletMap:React.FC=()=>{
-    useEffect(()=>{
-        const map = L.map('map',{
-            center:[46.23898,3.63389],
-            zoom:13
-        });
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{attribution:'© OpenStreetMap contributors'
 
-        }).addTo(map);
-    },[]);
+const LeafletMap=()=>{
+    
+    const [mapCenter, setMapCenter]=useState<[number,number]>([46.2401341,3.6339728]);
+    const [selectedType, setSelectedType]=useState("Tous");
+    const getCustomIcon=(type)=>{
+        return new Icon(iconMappings[type] || iconMappings.town);
 
-    return <div id="map" style={{height:'500px',width:'100%'}}/>
-};
+    };
+    const typeOption=["Tous",...Object.keys(iconMappings).sort()];
+    const fliteredMarkers=townMarker.filter(
+        (marker)=>selectedType==="Tous" || marker.type === selectedType ||
+        marker.type === "town"
+    );
+    const handleTypeChange=(e)=>{
+        setSelectedType(e.target.value);
+    };
+
+
+    return (
+        <section className='info-carte'>
+            <div>
+                <label htmlFor='filter'>Filtrer par : </label>
+                <select id='filter' onChange={handleTypeChange} value={selectedType}>
+                {...typeOption.map((type)=>(
+                    <option key={type} value={type}>{type}</option> ))};
+                </select>
+            </div>
+            <MapContainer 
+            center={mapCenter}
+            zoom={13}
+            style={{height:"500px",width:"100%"}}>
+                <TileLayer 
+                url="https://tile.openstreetmap.fr/?layers=B00000000FFFFFF"/>
+                <Marker position={mapCenter} icon={getCustomIcon('town')}>
+                    <Popup>My bled</Popup>
+                </Marker>
+                <MarkerClusterGroup>
+                    {fliteredMarkers.map((city,index)=>{
+                        <Marker
+                        key={index}
+                        position={city.coodinates}
+                        icon={getCustomIcon(city.type)}></Marker>
+                    })}
+                </MarkerClusterGroup>
+            </MapContainer>
+        </section>
+    );
+}
 
 export default LeafletMap;
